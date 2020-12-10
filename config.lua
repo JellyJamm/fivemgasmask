a8d116b0c32f046b517ffbbaff006b94fc091dbe
JellyJamm <65462366+JellyJamm@users.noreply.github.com>
2020-12-10 11:51:42 -0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,14 @@
+--================================--
+--          GASMASK v2.0          --
+--          by JellyJam           --
+--      License: GNU GPL 3.0      --
+--================================--
+
+Config = {}
+
+Config.rolelist = {
+	-- This is an example. Feel free to add a new line or edit the one below.
+	['Gas Mask Certified'] = 786364946824560670,
+}
+
+Config.mask = 46 -- (46 by default) Change this to whichever mask you want to be equipped when using the command
