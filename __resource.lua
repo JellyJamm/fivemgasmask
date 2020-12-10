de016c28154434a0283de9da128a274c432ff58a
JellyJamm <65462366+JellyJamm@users.noreply.github.com>
2020-12-09 21:19:13 -0800

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,13 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_script "client.lua"
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
 
-server_script "server.lua"
+server_scripts {
+    'server.lua',
+    'config.lua'
+}
 
 dependency 'discord_perms'
