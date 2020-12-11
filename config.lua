543e5204e007d7f574679109a7d4ebb06d85c522
JellyJamm <65462366+JellyJamm@users.noreply.github.com>
2020-12-10 23:08:41 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,17 @@
 --================================--
---          GASMASK v2.0          --
+--          GASMASK v2.5          --
 --          by JellyJam           --
 --      License: GNU GPL 3.0      --
 --================================--
 
 Config = {}
 
-Config.rolelist = {
-	-- This is an example. Feel free to add a new line or edit the one below.
-	['Gas Mask Certified'] = 786364946824560670,
-}
-
 Config.mask = 46 -- (46 by default) Change this to whichever mask you want to be equipped when using the command
+
+Config.showhud = true -- Change to false if you do not want the status of the gasmask to be shown on the HUD
+
+-- If above is set to false, ignore everything below this line
+
+Config.hudx = .225 -- X value of hud (used for positioning across X axis)
+Config.hudy = .952 -- Y value of hud (used for positioning across Y axis)
+Config.hudscale = 0.4 -- Scale of hud
